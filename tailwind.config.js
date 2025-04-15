c504dc4074ca1227dec069d76153a31ebbc380e2
mezentsevs <mezentsevs@rambler.ru>
2025-04-15 11:51:24 +0400

    Add tailwind config; add tailwind assets

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.vue',
+        './resources/**/*.js',
+        './resources/**/*.ts',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
