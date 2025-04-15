df645096f970f57dcb36d55188605c2913a4fdf0
mezentsevs <mezentsevs@rambler.ru>
2025-04-15 12:08:33 +0400

    Add app blade

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Typing Trainer</title>
+    @vite('resources/js/app.ts')
+</head>
+<body>
+<div id="app"></div>
+</body>
+</html>
