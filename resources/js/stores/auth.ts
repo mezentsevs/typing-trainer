76580350089b96d732bfb207ebdf017807df4341
mezentsevs <mezentsevs@rambler.ru>
2025-04-15 11:56:25 +0400

    Add auth store

---
+++ resources/js/stores/auth.ts
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        user: null as { id: number; name: string; email: string } | null,
+        token: localStorage.getItem('token') || null,
+    }),
+    actions: {
+        async login(email: string, password: string) {
+            const response = await axios.post('/login', { email, password });
+            this.token = response.data.token;
+            this.user = response.data.user;
+            localStorage.setItem('token', this.token!);
+            axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+        },
+        async register(name: string, email: string, password: string, password_confirmation: string) {
+            const response = await axios.post('/register', { name, email, password, password_confirmation });
+            this.token = response.data.token;
+            this.user = response.data.user;
+            localStorage.setItem('token', this.token!);
+            axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+        },
+        async logout() {
+            await axios.post('/logout');
+            this.token = null;
+            this.user = null;
+            localStorage.removeItem('token');
+            delete axios.defaults.headers.common['Authorization'];
+        },
+    },
+});
