01ba6cc11955efd656d566100f156c6cdaaa82b4
mezentsevs <mezentsevs@rambler.ru>
2025-04-15 14:17:47 +0400

    Add ts config; update dependencies

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
@@ -9,7 +8,6 @@ export default defineConfig({
             input: ['resources/js/app.ts'],
             refresh: true,
         }),
-        tailwindcss(),
         vue(),
     ],
     resolve: {
