de7ccd35655fe6b73bd8a3b0977f9c0ef96b2dd4
mezentsevs <mezentsevs@rambler.ru>
2025-04-23 20:40:46 +0400

    Refactoring

--- resources/js/components/Progress.vue
+++ resources/js/components/Progress.vue
@@ -1,18 +1,14 @@
 <template>
-    <div class="w-1/2 mx-auto">
-        <div class="flex justify-between">
-            <div class="w-full text-center">
-                <div class="w-full bg-gray-200 rounded-full h-4 relative">
-                    <div
-                        class="h-4 rounded-full transition-all duration-300"
-                        :style="{
-                            width: `${progress}%`,
-                            backgroundColor: `rgb(${255 - (progress / 100) * (255 - 22)}, ${255 - (progress / 100) * (255 - 163)}, ${255 - (progress / 100) * (255 - 74)})`
-                        }"
-                    ></div>
-                    <p class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs font-bold">{{ progress }}%</p>
-                </div>
-            </div>
+    <div class="mt-4 w-full">
+        <div class="w-full bg-gray-200 rounded-full h-4 relative">
+            <div
+                class="h-4 rounded-full transition-all duration-300"
+                :style="{
+                        width: `${progress}%`,
+                        backgroundColor: `rgb(${255 - (progress / 100) * (255 - 22)}, ${255 - (progress / 100) * (255 - 163)}, ${255 - (progress / 100) * (255 - 74)})`
+                    }"
+            ></div>
+            <p class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs font-bold">{{ progress }}%</p>
         </div>
     </div>
 </template>
--- resources/js/components/Statistics.vue
+++ resources/js/components/Statistics.vue
@@ -18,22 +18,13 @@
                 <p class="text-xs opacity-50">Errors</p>
             </div>
         </div>
-        <div class="mt-4 w-full">
-            <div class="w-full bg-gray-200 rounded-full h-4 relative">
-                <div
-                    class="h-4 rounded-full transition-all duration-300"
-                    :style="{
-                        width: `${progress}%`,
-                        backgroundColor: `rgb(${255 - (progress / 100) * (255 - 22)}, ${255 - (progress / 100) * (255 - 163)}, ${255 - (progress / 100) * (255 - 74)})`
-                    }"
-                ></div>
-                <p class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs font-bold">{{ progress }}%</p>
-            </div>
-        </div>
+        <Progress :progress="progress" />
     </div>
 </template>
 
 <script lang="ts" setup>
+import Progress from '@/components/Progress.vue';
+
 defineProps<{
     language: string;
     time: number;
