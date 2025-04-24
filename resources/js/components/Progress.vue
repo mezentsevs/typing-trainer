f926b789973b6e764f32a816932dc53fee3ee0e6
mezentsevs <mezentsevs@rambler.ru>
2025-04-24 07:51:44 +0400

    Update progress bar color

--- resources/js/components/Progress.vue
+++ resources/js/components/Progress.vue
@@ -4,9 +4,9 @@
             <div
                 class="h-4 rounded-full transition-all duration-300"
                 :style="{
-                        width: `${progress}%`,
-                        backgroundColor: `rgb(${255 - (progress / 100) * (255 - 22)}, ${255 - (progress / 100) * (255 - 163)}, ${255 - (progress / 100) * (255 - 74)})`
-                    }"
+                    width: `${progress}%`,
+                    backgroundColor: `rgb(${255 - (progress / 100) * (255 - 34)}, ${255 - (progress / 100) * (255 - 197)}, ${255 - (progress / 100) * (255 - 94)})`
+                }"
             ></div>
             <p class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs font-bold">{{ progress }}%</p>
         </div>
