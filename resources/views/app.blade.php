0df7e6700e245b382901694e43b1c3a70ae29546
mezentsevs <mezentsevs@rambler.ru>
2025-04-30 14:21:08 +0400

    Fix code style

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Typing Trainer</title>
-    @vite('resources/js/app.ts')
-</head>
-<body>
-<div id="app"></div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Typing Trainer</title>
+        @vite('resources/js/app.ts')
+    </head>
+
+    <body>
+        <div id="app"></div>
+    </body>
 </html>
