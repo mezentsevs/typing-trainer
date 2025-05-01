b88809a791164a5ba003b6f0884e29a68ec1794a
mezentsevs <mezentsevs@rambler.ru>
2025-05-01 18:07:22 +0400

    Fix code style

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -22,6 +22,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
     const token = localStorage.getItem('token');
+
     if (to.meta.requiresAuth && !token) {
         next('/login');
     } else {
