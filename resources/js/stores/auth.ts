d5f60415376c326ef84ced876fb9979c72b01b6e
mezentsevs <mezentsevs@rambler.ru>
2025-05-01 18:18:36 +0400

    Fix code style

--- resources/js/stores/auth.ts
+++ resources/js/stores/auth.ts
@@ -9,22 +9,28 @@ export const useAuthStore = defineStore('auth', {
     actions: {
         async login(email: string, password: string) {
             const response = await axios.post('/login', { email, password });
+
             this.token = response.data.token;
             this.user = response.data.user;
+
             localStorage.setItem('token', this.token!);
             axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
         },
         async register(name: string, email: string, password: string, password_confirmation: string) {
             const response = await axios.post('/register', { name, email, password, password_confirmation });
+
             this.token = response.data.token;
             this.user = response.data.user;
+
             localStorage.setItem('token', this.token!);
             axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
         },
         async logout() {
             await axios.post('/logout');
+
             this.token = null;
             this.user = null;
+
             localStorage.removeItem('token');
             delete axios.defaults.headers.common['Authorization'];
         },
