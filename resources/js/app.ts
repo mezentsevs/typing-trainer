a88c9fbb2a901263b013497aa270b80b1ab2e87e
mezentsevs <mezentsevs@rambler.ru>
2025-05-01 08:42:05 +0400

    Fix code style

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -7,11 +7,12 @@ import { createPinia } from 'pinia';
 
 axios.defaults.baseURL = '/api';
 const token = localStorage.getItem('token');
+
 if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
-app.mount('#app');
+createApp(App)
+    .use(createPinia())
+    .use(router)
+    .mount('#app');
