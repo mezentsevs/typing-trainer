f3809d55600964677885331499a00b2638cee6f2
mezentsevs <mezentsevs@rambler.ru>
2025-05-09 10:52:43 +0400

    Fix code style

--- resources/js/interfaces/LessonsProgressPayloadInterface.ts
+++ resources/js/interfaces/LessonsProgressPayloadInterface.ts
@@ -1,7 +1,7 @@
 export interface LessonsProgressPayloadInterface {
-    lesson_id: number;
+    lessonId: number;
     language: string;
-    time_seconds: number;
-    speed_wpm: number;
+    timeSeconds: number;
+    speedWpm: number;
     errors: number;
 }
