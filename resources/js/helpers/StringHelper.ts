fa30316b03675b7a4caace9b059897628533e769
mezentsevs <mezentsevs@rambler.ru>
2025-05-22 09:51:06 +0400

    Refactor

--- resources/js/helpers/StringHelper.ts
+++ resources/js/helpers/StringHelper.ts
@@ -7,19 +7,16 @@ function isSeparator(char: string): boolean {
 export function getCurrentTypingUnit(text: string, pos: number): TypingUnitInterface | null {
     if (pos >= text.length) { return null; }
 
-    if (isSeparator(text[pos])) {
-        let start = pos;
-        while (start > 0 && isSeparator(text[start - 1])) { start--; }
+    let start: number = pos;
+    let end: number = pos;
 
-        let end = pos;
+    if (isSeparator(text[pos])) {
+        while (start > 0 && isSeparator(text[start - 1])) { start--; }
         while (end < text.length - 1 && isSeparator(text[end + 1])) { end++; }
 
         return { start, end };
     } else {
-        let start = pos;
         while (start > 0 && !isSeparator(text[start - 1])) { start--; }
-
-        let end = pos;
         while (end < text.length - 1 && !isSeparator(text[end + 1])) { end++; }
 
         return { start, end };
