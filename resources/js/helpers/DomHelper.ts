55e3dfef4ed446d88f963087dc0a84d7ac9a44c5
mezentsevs <mezentsevs@rambler.ru>
2025-05-22 09:26:37 +0400

    Refactor

--- resources/js/helpers/DomHelper.ts
+++ resources/js/helpers/DomHelper.ts
@@ -1,12 +1,12 @@
 export function scrollToCurrentChar (textContainer: HTMLElement | null, currentIndex: number): void {
     if (!textContainer) { return; }
 
-    const charElements = textContainer.querySelectorAll('span');
+    const charElements: NodeListOf<HTMLSpanElement> = textContainer.querySelectorAll('span');
 
     if (charElements[currentIndex]) {
-        const charRect = charElements[currentIndex].getBoundingClientRect();
-        const containerRect = textContainer.getBoundingClientRect();
-        const offset = charRect.top - containerRect.top + textContainer.scrollTop;
+        const charRect: DOMRect = charElements[currentIndex].getBoundingClientRect();
+        const containerRect: DOMRect = textContainer.getBoundingClientRect();
+        const offset: number = charRect.top - containerRect.top + textContainer.scrollTop;
 
         textContainer.scrollTo({ top: offset - containerRect.height / 2, behavior: 'smooth' });
     }
