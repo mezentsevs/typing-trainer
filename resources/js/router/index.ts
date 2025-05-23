b4b627c408a7c8f0a99c2a83481aa1ca98d0c192
mezentsevs <mezentsevs@rambler.ru>
2025-05-23 09:46:25 +0400

    Refactor

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -4,9 +4,19 @@ import Lesson from '@/components/Lesson.vue';
 import LessonSetup from '@/components/LessonSetup.vue';
 import Login from '@/components/Login.vue';
 import Register from '@/components/Register.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+    NavigationGuardNext,
+    NavigationGuardReturn,
+    RouteLocationNormalized,
+    RouteLocationNormalizedLoaded,
+    RouteRecordRaw,
+    Router,
+    _Awaitable,
+    createRouter,
+    createWebHistory,
+} from 'vue-router';
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     { path: '/', component: Home, meta: { requiresAuth: true } },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
@@ -15,13 +25,17 @@ const routes = [
     { path: '/test/:language', component: FinalTest, meta: { requiresAuth: true } },
 ];
 
-const router = createRouter({
+const router: Router = createRouter({
     history: createWebHistory(),
     routes,
 });
 
-router.beforeEach((to, from, next) => {
-    const token = localStorage.getItem('token');
+router.beforeEach((
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalizedLoaded,
+    next: NavigationGuardNext,
+): _Awaitable<NavigationGuardReturn> => {
+    const token: string | null = localStorage.getItem('token');
 
     if (to.meta.requiresAuth && !token) {
         next('/login');
