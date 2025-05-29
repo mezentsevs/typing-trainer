6d60b600e7762691a0832ab2c11110039d051530
mezentsevs <mezentsevs@rambler.ru>
2025-05-29 09:08:39 +0400

    Add attributes to register form

--- resources/js/components/Register.vue
+++ resources/js/components/Register.vue
@@ -4,20 +4,20 @@
             <h2 class="text-2xl font-bold mb-6">Register</h2>
             <form @submit.prevent="register">
                 <div class="mb-4">
-                    <label class="block text-gray-700">Name</label>
-                    <input v-model="form.name" type="text" class="w-full p-2 border rounded" required />
+                    <label for="name" class="block text-gray-700">Name</label>
+                    <input id="name" v-model="form.name" type="text" class="w-full p-2 border rounded" required autocomplete="on" />
                 </div>
                 <div class="mb-4">
-                    <label class="block text-gray-700">Email</label>
-                    <input v-model="form.email" type="email" class="w-full p-2 border rounded" required />
+                    <label for="email" class="block text-gray-700">Email</label>
+                    <input id="email" v-model="form.email" type="email" class="w-full p-2 border rounded" required autocomplete="on" />
                 </div>
                 <div class="mb-4">
-                    <label class="block text-gray-700">Password</label>
-                    <input v-model="form.password" type="password" class="w-full p-2 border rounded" required />
+                    <label for="password" class="block text-gray-700">Password</label>
+                    <input id="password" v-model="form.password" type="password" class="w-full p-2 border rounded" required />
                 </div>
                 <div class="mb-4">
-                    <label class="block text-gray-700">Confirm Password</label>
-                    <input v-model="form.passwordConfirmation" type="password" class="w-full p-2 border rounded" required />
+                    <label for="passwordConfirmation" class="block text-gray-700">Confirm Password</label>
+                    <input id="passwordConfirmation" v-model="form.passwordConfirmation" type="password" class="w-full p-2 border rounded" required />
                 </div>
                 <button type="submit" class="w-full bg-blue-500 text-white p-2 rounded">Register</button>
             </form>
