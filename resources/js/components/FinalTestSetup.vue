fd761ba4c0da13e4563aedfc48303b21402bb3a0
mezentsevs <mezentsevs@rambler.ru>
2025-05-30 09:28:08 +0400

    Add attributes to final test setup form

--- resources/js/components/FinalTestSetup.vue
+++ resources/js/components/FinalTestSetup.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-1/2 mx-auto my-4 border border-opacity-50 rounded-lg p-4">
         <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold">Genre (optional)</label>
-            <select v-model="genre" class="p-2 border rounded w-full text-sm">
+            <label for="genre" class="block text-gray-700 text-sm font-bold">Genre (optional)</label>
+            <select id="genre" v-model="genre" class="p-2 border rounded w-full text-sm">
                 <option value="">None</option>
                 <option value="fiction">Fiction</option>
                 <option value="non-fiction">Non-fiction</option>
@@ -10,8 +10,8 @@
             </select>
         </div>
         <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold">Upload Text (max 3 KB, optional)</label>
-            <input type="file" accept=".txt" @change="uploadFile" class="p-2 border rounded w-full text-sm" />
+            <label for="file" class="block text-gray-700 text-sm font-bold">Upload Text (max 3 KB, optional)</label>
+            <input id="file" type="file" accept=".txt" @change="uploadFile" class="p-2 border rounded w-full text-sm" />
         </div>
         <button @click="$emit('start', genre)" class="bg-blue-500 text-white p-2 rounded w-full text-sm">
             Start
