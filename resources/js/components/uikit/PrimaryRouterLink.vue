9709f9037bac7eb5f51f20ff91f5d0e870c35900
mezentsevs <mezentsevs@rambler.ru>
2025-06-04 08:30:53 +0400

    Add primary router link component

---
+++ resources/js/components/uikit/PrimaryRouterLink.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-link :to class="px-4 py-2 bg-blue-500 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-center text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-50 transition ease-in-out duration-150">
+        <slot />
+    </router-link>
+</template>
+
+<script lang="ts" setup>
+import { UIKitRouterLinkToType } from '@/types/UIKitTypes';
+
+withDefaults(defineProps<{
+    to?: UIKitRouterLinkToType,
+}>(), {
+    to: '/',
+});
+</script>
--- resources/js/types/UIKitTypes.ts
+++ resources/js/types/UIKitTypes.ts
@@ -1,3 +1,5 @@
 import { UIKitButtonTypeEnum } from '@/enums/UIKitEnums';
 
 export type UIKitButtonType = UIKitButtonTypeEnum.Button | UIKitButtonTypeEnum.Reset | UIKitButtonTypeEnum.Submit | undefined;
+
+export type UIKitRouterLinkToType = string | Object;
