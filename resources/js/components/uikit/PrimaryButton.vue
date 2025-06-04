d3130d5a15a61d5f2d2269ca3f4ba9c4952d934c
mezentsevs <mezentsevs@rambler.ru>
2025-06-04 10:09:14 +0400

    Add base button; refactor

--- resources/js/components/uikit/PrimaryButton.vue
+++ resources/js/components/uikit/PrimaryButton.vue
@@ -1,10 +1,11 @@
 <template>
-    <button :type class="px-4 py-2 bg-blue-500 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-50 transition ease-in-out duration-150">
+    <BaseButton :type class="bg-blue-500 dark:bg-gray-200">
         <slot />
-    </button>
+    </BaseButton>
 </template>
 
 <script lang="ts" setup>
+import BaseButton from '@/components/uikit/BaseButton.vue';
 import { UIKitButtonType } from '@/types/UIKitTypes';
 import { UIKitButtonTypeEnum } from '@/enums/UIKitEnums';
 
