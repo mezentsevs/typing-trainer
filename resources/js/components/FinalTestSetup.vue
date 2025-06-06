cc02ca435905cbc4abae00804c407b385eeb107d
mezentsevs <mezentsevs@rambler.ru>
2025-06-06 09:32:45 +0400

    Update final test setup form labels

--- resources/js/components/FinalTestSetup.vue
+++ resources/js/components/FinalTestSetup.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-1/2 mx-auto my-4 border border-opacity-50 rounded-lg p-4">
         <div class="mb-4">
-            <label for="genre" class="block text-gray-700">Genre <Remark text="optional" /></label>
+            <InputLabel for="genre">Genre <Remark value="(optional)" /></InputLabel>
             <select id="genre" v-model="genre" class="p-2 border rounded w-full">
                 <option value="">None</option>
                 <option value="fiction">Fiction</option>
@@ -10,7 +10,7 @@
             </select>
         </div>
         <div class="mb-4">
-            <label for="file" class="block text-gray-700">Text <Remark text="optional, max 3 KB" /></label>
+            <InputLabel for="file">Text <Remark value="(optional, max 3 KB)" /></InputLabel>
             <input id="file" type="file" accept=".txt" @change="uploadFile" class="p-2 border rounded w-full" />
         </div>
         <PrimaryButton @click="$emit('start', genre)" class="w-full">Start</PrimaryButton>
@@ -20,6 +20,7 @@
 
 <script lang="ts" setup>
 import ErrorMessage from '@/components/uikit/ErrorMessage.vue';
+import InputLabel from '@/components/uikit/InputLabel.vue';
 import PrimaryButton from '@/components/uikit/PrimaryButton.vue';
 import Remark from '@/components/uikit/Remark.vue';
 import { Ref, ref } from 'vue';
