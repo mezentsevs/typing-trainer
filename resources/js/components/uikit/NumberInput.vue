3b8fae86483b6bc8bb4d6cd4b55907b303e747f8
mezentsevs <mezentsevs@rambler.ru>
2025-06-10 09:38:26 +0400

    Add number input component

---
+++ resources/js/components/uikit/NumberInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <input
+        type="number"
+        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        :value="modelValue"
+        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    >
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    modelValue: number,
+}>();
+
+defineEmits(['update:modelValue']);
+</script>
