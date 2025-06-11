266225065e8e8a92491a70a9156edbefe80fb826
mezentsevs <mezentsevs@rambler.ru>
2025-06-11 08:29:27 +0400

    Refactor

--- resources/js/components/uikit/NumberInput.vue
+++ resources/js/components/uikit/NumberInput.vue
@@ -1,16 +1,11 @@
 <template>
     <input
+        v-model="model"
         type="number"
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-        :value="modelValue"
-        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
     >
 </template>
 
 <script lang="ts" setup>
-defineProps<{
-    modelValue: number,
-}>();
-
-defineEmits(['update:modelValue']);
+const model = defineModel();
 </script>
