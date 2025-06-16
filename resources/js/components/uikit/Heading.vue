2f56eb625fe641c11c0da41876566acf0d060fc2
mezentsevs <mezentsevs@rambler.ru>
2025-06-16 09:40:45 +0400

    Add heading component

---
+++ resources/js/components/uikit/Heading.vue
@@ -0,0 +1,25 @@
+<template>
+    <component :is="heading" class="font-bold text-gray-800 dark:text-gray-200 truncate">
+        <slot />
+    </component>
+</template>
+
+<script lang="ts" setup>
+import { computed, ComputedRef } from 'vue';
+
+const props = withDefaults(defineProps<{
+    level?: number,
+}>(), {
+    level: 1,
+});
+
+const heading: ComputedRef<string> = computed((): string => {
+    if (props.level < 1) {
+        return 'h1';
+    } else if (props.level > 6) {
+        return 'h6';
+    } else {
+        return `h${props.level}`;
+    }
+});
+</script>
