796737974814f2a29d84ec4a719ccb9c95812e7b
mezentsevs <mezentsevs@rambler.ru>
2025-06-24 10:23:53 +0400

    Add rest fields to lesson interface

--- resources/js/interfaces/LessonInterface.ts
+++ resources/js/interfaces/LessonInterface.ts
@@ -1,5 +1,13 @@
+import { KeyboardLanguageEnum } from '@/enums/KeyboardEnums';
+
 export default interface LessonInterface {
     id: number;
+    user_id: number;
     number: number;
+    total: number;
+    language: KeyboardLanguageEnum;
     newChars: string;
+    text: string;
+    created_at: string;
+    updated_at: string;
 }
