57168289850ba848ceee3cae61c7c6444aeee32f
mezentsevs <mezentsevs@rambler.ru>
2025-07-01 10:48:27 +0400

    Add app layout

---
+++ resources/js/layouts/AppLayout.vue
@@ -0,0 +1,9 @@
+<template>
+    <BaseLayout class="min-h-screen flex items-center justify-center">
+        <slot />
+    </BaseLayout>
+</template>
+
+<script lang="ts" setup>
+import BaseLayout from '@/layouts/BaseLayout.vue';
+</script>
