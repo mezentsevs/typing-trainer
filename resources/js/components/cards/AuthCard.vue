bbc7abedee67523b684cea254c10d98ad4591ca2
mezentsevs <mezentsevs@rambler.ru>
2025-07-04 11:37:14 +0400

    Update base card component; refactor auth card component

--- resources/js/components/cards/AuthCard.vue
+++ resources/js/components/cards/AuthCard.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="w-80 min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-        <div>
+    <BaseCard class="w-80">
+        <template #base-card-logo>
             <slot name="logo" />
-        </div>
+        </template>
 
-        <div class="w-full mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-            <slot />
-        </div>
-    </div>
+        <slot />
+    </BaseCard>
 </template>
+
+<script lang="ts" setup>
+import BaseCard from '@/components/cards/BaseCard.vue';
+</script>
