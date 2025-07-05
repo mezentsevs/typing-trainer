8879523ab2e9e7ee3ffd8523ab1c3caf5a87f9a5
mezentsevs <mezentsevs@rambler.ru>
2025-07-05 14:24:47 +0400

    Add text area and container borders

--- resources/js/components/uikit/TextArea.vue
+++ resources/js/components/uikit/TextArea.vue
@@ -2,7 +2,7 @@
     <textarea
         ref="area"
         v-model="model"
-        class="p-2 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 rounded shadow-sm"
+        class="p-2 border border-opacity-50 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 rounded shadow-sm"
     />
 </template>
 
