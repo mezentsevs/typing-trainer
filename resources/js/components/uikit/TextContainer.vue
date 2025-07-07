f5ebef7ba2eaf4fda77f2d4114af6956b72a04d9
mezentsevs <mezentsevs@rambler.ru>
2025-07-07 21:14:58 +0400

    Change section to article tag on text container component

--- resources/js/components/uikit/TextContainer.vue
+++ resources/js/components/uikit/TextContainer.vue
@@ -1,10 +1,10 @@
 <template>
-    <section
+    <article
         ref="container"
         class="p-2 border border-opacity-50 border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-900 dark:text-gray-300 overflow-y-auto break-words whitespace-pre-wrap rounded shadow-sm"
     >
         <slot />
-    </section>
+    </article>
 </template>
 
 <script lang="ts" setup>
