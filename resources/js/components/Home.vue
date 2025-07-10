2a6af5d152d5799b5ca17e54f92ce1a69480b17f
mezentsevs <mezentsevs@rambler.ru>
2025-07-10 13:59:29 +0400

    Refactor home component

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -10,13 +10,13 @@
         <div class="mt-12 flex flex-col items-center gap-6">
             <router-link
                 to="/setup"
-                class="w-48 py-2 px-4 bg-transparent hover:bg-cyan-500/10 border-2 border-cyan-500 text-lg font-mono text-cyan-400 hover:text-cyan-300 transition-all duration-200 ease-in-out text-center shadow-[0_0_10px_0_rgba(6,182,212,0.5)] hover:shadow-[0_0_15px_0_rgba(6,182,212,0.7)] rounded-lg"
+                :class="[COMMON_BUTTON_CLASS, START_BUTTON_CLASS]"
             >
                 Start
             </router-link>
             <button
                 @click="logout"
-                class="w-48 py-2 px-4 bg-transparent hover:bg-red-500/10 border-2 border-red-500 text-lg font-mono text-red-400 hover:text-red-300 transition-all duration-200 ease-in-out text-center shadow-[0_0_10px_0_rgba(239,68,68,0.5)] hover:shadow-[0_0_15px_0_rgba(239,68,68,0.7)] rounded-lg"
+                :class="[COMMON_BUTTON_CLASS, LOGOUT_BUTTON_CLASS]"
             >
                 Logout
             </button>
@@ -50,6 +50,9 @@ const CIRCLES: Record<string, string>[] = [
     { class: 'bottom-20 right-20 w-32 h-32 bg-purple-500 delay-1000' },
     { class: 'top-1/2 left-1/4 w-16 h-16 bg-red-500 delay-500' },
 ];
+const COMMON_BUTTON_CLASS = 'w-48 py-2 px-4 bg-transparent border-2 text-lg font-mono transition-all duration-200 ease-in-out text-center rounded-lg';
+const LOGOUT_BUTTON_CLASS = 'border-red-500 text-red-400 hover:text-red-300 hover:bg-red-500/10 shadow-[0_0_10px_0_rgba(239,68,68,0.5)] hover:shadow-[0_0_15px_0_rgba(239,68,68,0.7)]';
+const START_BUTTON_CLASS = 'border-cyan-500 text-cyan-400 hover:text-cyan-300 hover:bg-cyan-500/10 shadow-[0_0_10px_0_rgba(6,182,212,0.5)] hover:shadow-[0_0_15px_0_rgba(6,182,212,0.7)]';
 const currentText: Ref<string> = ref('');
 
 const logout = async (): Promise<void> => {
