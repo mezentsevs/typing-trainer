a1493ade0a03f2d127cd5be2f8cf5676bf841090
mezentsevs <mezentsevs@rambler.ru>
2025-08-14 22:41:13 +0400

    Add testGenerateLessonsDeletesExistingAndCreatesNew

--- tests/Unit/Services/LessonServiceTest.php
+++ tests/Unit/Services/LessonServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\Services;
 
+use App\Models\Lesson;
+use App\Models\LessonResult;
 use App\Models\User;
 use App\Services\LessonService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -24,5 +26,64 @@ class LessonServiceTest extends TestCase
         $this->user = User::factory()->create();
         $this->service = new LessonService();
         $this->reflection = new ReflectionClass($this->service);
+    }
+
+    public function testGenerateLessonsDeletesExistingAndCreatesNew(): void
+    {
+        $language = 'en';
+        $lessonCount = 5;
+        $existingLessonCount = 3;
+        $existingLessonResultCount = 3;
+
+        $existingLessons = Lesson::factory()->count($existingLessonCount)->create([
+            'user_id' => $this->user->id,
+            'language' => $language,
+        ]);
+
+        $existingLessonResults = LessonResult::factory()->count($existingLessonResultCount)->create([
+            'user_id' => $this->user->id,
+            'language' => $language,
+        ]);
+
+        $this->service->generateLessons($language, $lessonCount, $this->user->id);
+
+        foreach ($existingLessons as $lesson) {
+            $this->assertDatabaseMissing('lessons', [
+                'id' => $lesson->id,
+            ]);
+        }
+
+        foreach ($existingLessonResults as $lessonResult) {
+            $this->assertDatabaseMissing('lesson_results', [
+                'id' => $lessonResult->id,
+            ]);
+        }
+
+        $newLessons = Lesson::where('user_id', $this->user->id)
+            ->where('language', $language)
+            ->get();
+
+        $this->assertCount($lessonCount, $newLessons);
+
+        foreach ($newLessons as $lesson) {
+            $this->assertInstanceOf(Lesson::class, $lesson);
+            $this->assertEquals($this->user->id, $lesson->user_id);
+            $this->assertEquals($language, $lesson->language);
+
+            $this->assertIsInt($lesson->number);
+            $this->assertGreaterThanOrEqual(1, $lesson->number);
+            $this->assertLessThanOrEqual($lessonCount, $lesson->number);
+
+            $this->assertIsInt($lesson->total);
+            $this->assertEquals($lessonCount, $lesson->total);
+
+            $this->assertIsString($lesson->new_chars);
+            $this->assertNotEmpty($lesson->new_chars);
+
+            $this->assertIsString($lesson->text);
+            $this->assertNotEmpty($lesson->text);
+            $this->assertGreaterThanOrEqual(1, mb_strlen($lesson->text));
+            $this->assertLessThanOrEqual($this->reflection->getConstant('MAX_LESSON_LENGTH'), mb_strlen($lesson->text));
+        }
     }
 }
