8c4f9fe5fed04b7ff40cb5c1d2ba7502cc58768e
mezentsevs <mezentsevs@rambler.ru>
2025-08-15 12:31:50 +0400

    Add trailing comma in multiline php cs fixer option

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -26,6 +26,14 @@ return new Config()
         'no_whitespace_before_comma_in_array' => true,
         'whitespace_after_comma_in_array' => true,
         'no_unneeded_control_parentheses' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => [
+                'arrays',
+                'arguments',
+                'parameters',
+            ],
+            'after_heredoc' => true,
+        ],
     ])
     ->setFinder($finder)
     ->setRiskyAllowed(true)
