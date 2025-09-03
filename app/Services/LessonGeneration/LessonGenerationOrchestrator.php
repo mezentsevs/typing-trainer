8f9c5d51a7ab5d9c1d4ce18966d24aae7a4a263c
mezentsevs <mezentsevs@rambler.ru>
2025-09-03 23:35:41 +0400

    Refactor: split deleteLessonsAndResults method

--- app/Services/LessonGeneration/LessonGenerationOrchestrator.php
+++ app/Services/LessonGeneration/LessonGenerationOrchestrator.php
@@ -19,7 +19,8 @@ class LessonGenerationOrchestrator
      */
     public function generate(string $language, int $lessonCount, int $userId): void
     {
-        $this->deleteLessonsAndResults($language, $userId);
+        $this->deleteLessons($language, $userId);
+        $this->deleteLessonResults($language, $userId);
 
         $lessonBlueprints = $this->lessonSequenceGenerator->generate($language, $lessonCount);
 
@@ -31,12 +32,15 @@ class LessonGenerationOrchestrator
         Lesson::insert($lessons);
     }
 
-    protected function deleteLessonsAndResults(string $language, int $userId): void
+    protected function deleteLessons(string $language, int $userId): void
     {
         Lesson::where('user_id', $userId)
             ->where('language', $language)
             ->delete();
+    }
 
+    protected function deleteLessonResults(string $language, int $userId): void
+    {
         LessonResult::where('user_id', $userId)
             ->where('language', $language)
             ->delete();
