63599619d15621cee8289bd46aff662976a2d1f4
mezentsevs <mezentsevs@rambler.ru>
2025-10-06 10:14:32 +0400

    style(test): remove spaces

--- tests/Feature/Web/HomePageTest.php
+++ tests/Feature/Web/HomePageTest.php
@@ -7,7 +7,7 @@ use Tests\TestCase;
 class HomePageTest extends TestCase
 {
     private const string HOME_URI = '/';
-    
+
     public function testApplicationReturnsSuccessfulResponse(): void
     {
         $response = $this->get(self::HOME_URI);
