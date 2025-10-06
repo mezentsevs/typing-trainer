905053b76ff52bf8b3d98f54c745efd73980afb0
mezentsevs <mezentsevs@rambler.ru>
2025-10-06 09:30:04 +0400

    test: add home uri const

--- tests/Feature/Web/HomePageTest.php
+++ tests/Feature/Web/HomePageTest.php
@@ -6,9 +6,12 @@ use Tests\TestCase;
 
 class HomePageTest extends TestCase
 {
+    private const string HOME_URI = '/';
+    
     public function testApplicationReturnsSuccessfulResponse(): void
     {
-        $response = $this->get('/');
+        $response = $this->get(self::HOME_URI);
+
         $response->assertStatus(200);
     }
 }
