e0b44a2948eb4f02788e646d049d5f1ba1015740
mezentsevs <mezentsevs@rambler.ru>
2025-10-15 09:36:08 +0400

    test: update testRegistrationWithValidUserName

--- tests/Feature/Api/Auth/RegistrationTest.php
+++ tests/Feature/Api/Auth/RegistrationTest.php
@@ -134,7 +134,11 @@ class RegistrationTest extends TestCase
             'password_confirmation' => self::PASSWORD,
         ]);
 
-        $response->assertStatus(201);
+        $this->withResponse($response)
+            ->assertStatusWithJsonStructure(201, [
+                'token',
+                'user' => self::REGISTER_RESPONSE_USER_JSON_STRUCTURE,
+            ]);
     }
 
     #[DataProviderExternal(AuthDataProvider::class, 'provideInvalidUserNames')]
