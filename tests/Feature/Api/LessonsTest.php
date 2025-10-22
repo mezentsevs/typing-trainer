22f0f2cfdbf91cace282ce1d6654f6858dc35945
mezentsevs <mezentsevs@rambler.ru>
2025-10-22 09:21:40 +0400

    test: add testLessonsResultSaveWithUnknownLanguage

--- tests/Feature/Api/LessonsTest.php
+++ tests/Feature/Api/LessonsTest.php
@@ -185,25 +185,21 @@ class LessonsTest extends TestCase
     }
 
     #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
-    public function testLessonsResultSaveValidation(string $language): void
+    public function testLessonsResultSaveWithUnknownLanguage(string $language): void
     {
         $lesson = $this->createLesson($this->user, ['language' => $language]);
 
         $response = $this->withToken($this->token)
             ->postJson('/api/lessons/result', [
                 'lesson_id' => $lesson->id,
-                'language' => $language,
-                'time_seconds' => self::INVALID_TIME_SECONDS,
-                'speed_wpm' => self::INVALID_SPEED_WPM,
-                'errors' => self::INVALID_ERRORS_COUNT,
+                'language' => Language::Unknown->value,
+                'time_seconds' => self::TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::ERRORS_COUNT,
             ]);
 
         $this->withResponse($response)
-            ->assertStatusWithJsonValidationErrors(422, [
-                'time_seconds',
-                'speed_wpm',
-                'errors',
-            ]);
+            ->assertStatusWithErrorAndMessage(422, 'language', 'The selected language is not supported.');
     }
 
     #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
