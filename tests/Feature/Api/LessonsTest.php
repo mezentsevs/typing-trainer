33b36d0589619bec191a62518daa9d1f902f3f43
mezentsevs <mezentsevs@rambler.ru>
2025-10-28 14:38:27 +0400

    refactor(test): move testLessonsResultSaveWithUnknownLanguage

--- tests/Feature/Api/LessonsTest.php
+++ tests/Feature/Api/LessonsTest.php
@@ -304,6 +304,24 @@ class LessonsTest extends TestCase
     }
 
     #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
+    public function testLessonsResultSaveWithUnknownLanguage(string $language): void
+    {
+        $lesson = $this->createLesson($this->user, ['language' => $language]);
+
+        $response = $this->withToken($this->token)
+            ->postJson(self::LESSONS_RESULT_URI, [
+                'lesson_id' => $lesson->id,
+                'language' => Language::Unknown->value,
+                'time_seconds' => self::TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::ERRORS_COUNT,
+            ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithErrorAndMessage(422, 'language', 'The selected language is not supported.');
+    }
+
+    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
     public function testLessonsResultSaveWithZeroTimeSeconds(string $language): void
     {
         $lesson = $this->createLesson($this->user, ['language' => $language]);
@@ -355,24 +373,6 @@ class LessonsTest extends TestCase
 
         $this->withResponse($response)
             ->assertStatusWithJsonStructure(200, self::LESSONS_RESULT_RESPONSE_JSON_STRUCTURE);
-    }
-
-    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
-    public function testLessonsResultSaveWithUnknownLanguage(string $language): void
-    {
-        $lesson = $this->createLesson($this->user, ['language' => $language]);
-
-        $response = $this->withToken($this->token)
-            ->postJson(self::LESSONS_RESULT_URI, [
-                'lesson_id' => $lesson->id,
-                'language' => Language::Unknown->value,
-                'time_seconds' => self::TIME_SECONDS,
-                'speed_wpm' => self::SPEED_WPM,
-                'errors' => self::ERRORS_COUNT,
-            ]);
-
-        $this->withResponse($response)
-            ->assertStatusWithErrorAndMessage(422, 'language', 'The selected language is not supported.');
     }
 
     #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
