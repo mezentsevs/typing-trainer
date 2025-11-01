60fa2479da4e596c276dc20e9572ac00f9b762d8
mezentsevs <mezentsevs@rambler.ru>
2025-11-01 09:31:05 +0400

    test: add testLessonsResultSaveWithNonIntegerSpeedWpm

--- tests/Traits/Constants/WithStatisticsConstants.php
+++ tests/Traits/Constants/WithStatisticsConstants.php
@@ -4,16 +4,17 @@ namespace Tests\Traits\Constants;
 
 trait WithStatisticsConstants
 {
-    protected const int ERRORS_COUNT = 2;
-    protected const int ZERO_ERRORS_COUNT = 0;
-    protected const int INVALID_INT_ERRORS_COUNT = -1;
-
-    protected const int SPEED_WPM = 50;
-    protected const int ZERO_SPEED_WPM = 0;
-    protected const int INVALID_INT_SPEED_WPM = -1;
-
     protected const int TIME_SECONDS = 60;
     protected const int ZERO_TIME_SECONDS = 0;
     protected const int INVALID_INT_TIME_SECONDS = -1;
     protected const string INVALID_STRING_TIME_SECONDS = 'invalidStringTimeSeconds';
+
+    protected const int SPEED_WPM = 50;
+    protected const int ZERO_SPEED_WPM = 0;
+    protected const int INVALID_INT_SPEED_WPM = -1;
+    protected const string INVALID_STRING_SPEED_WPM = 'invalidStringSpeedWpm';
+
+    protected const int ERRORS_COUNT = 2;
+    protected const int ZERO_ERRORS_COUNT = 0;
+    protected const int INVALID_INT_ERRORS_COUNT = -1;
 }
