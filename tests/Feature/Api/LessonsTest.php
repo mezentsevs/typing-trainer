52fb21a47a6fe58a79aa361fcf241866006d77e7
mezentsevs <mezentsevs@rambler.ru>
2025-11-05 12:12:57 +0400

    test: add testLessonsShowWithBelowMinLessonNumber

--- tests/Feature/Api/LessonsTest.php
+++ tests/Feature/Api/LessonsTest.php
@@ -302,6 +302,18 @@ class LessonsTest extends TestCase
     }
 
     #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
+    public function testLessonsShowWithBelowMinLessonNumber(string $language): void
+    {
+        $lessonUri = sprintf(self::LESSONS_SHOW_URI_TEMPLATE, $language, self::MIN_LESSON_COUNT - 1);
+
+        $response = $this->withToken($this->token)
+            ->getJson($lessonUri);
+
+        $this->withResponse($response)
+            ->assertStatusWithErrorAndMessage(422, 'lesson_number', 'The lesson number field must be at least 1.');
+    }
+
+    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
     public function testLessonsResultSaveSuccess(string $language): void
     {
         $lesson = $this->createLesson($this->user, ['language' => $language]);
