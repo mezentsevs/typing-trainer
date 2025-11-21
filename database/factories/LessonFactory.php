b894589cf5aea938718415c41a5ae523330ceb2a
mezentsevs <mezentsevs@rambler.ru>
2025-11-21 12:38:08 +0400

    refactor: add NEW_CHARS_REGEX constant to LessonFactory

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -16,6 +16,7 @@ class LessonFactory extends Factory
     use WithLessonConstants;
 
     protected const int TEXT_WORDS_COUNT = 10;
+    protected const string NEW_CHARS_REGEX = '[a-zA-Z0-9]{5}';
 
     protected $model = Lesson::class;
 
@@ -30,7 +31,7 @@ class LessonFactory extends Factory
             'total' => $this->faker->numberBetween($number, self::MAX_LESSON_COUNT),
             'language' => $this->faker->randomElement([Language::En->value, Language::Ru->value]),
             // TODO: Implement custom new_chars fake method - Faker doesn't work correctly with Cyrillic
-            'new_chars' => $this->faker->regexify('[a-zA-Z0-9]{5}'),
+            'new_chars' => $this->faker->regexify(self::NEW_CHARS_REGEX),
             'text' => $this->faker->sentence(self::TEXT_WORDS_COUNT, true),
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
             'updated_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
