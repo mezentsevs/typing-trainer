eff9ffe6bb910d2401f56ad32a2980f5b7c83970
mezentsevs <mezentsevs@rambler.ru>
2025-11-21 11:54:40 +0400

    chore: add TODO comment to LessonFactory for faker Cyrillic fix

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -27,6 +27,7 @@ class LessonFactory extends Factory
             'number' => $number = $this->faker->numberBetween(self::MIN_LESSON_COUNT, self::MAX_LESSON_COUNT),
             'total' => $this->faker->numberBetween($number, self::MAX_LESSON_COUNT),
             'language' => $this->faker->randomElement([Language::En->value, Language::Ru->value]),
+            // TODO: Implement custom new_chars fake method - Faker doesn't work correctly with Cyrillic
             'new_chars' => $this->faker->regexify('[a-z]{5}'),
             'text' => $this->faker->sentence(10, true),
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
