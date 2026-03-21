461b36cb36d62712a7de07581a6d09ca7cb810f8
mezentsevs <mezentsevs@rambler.ru>
2026-03-21 11:10:41 +0400

    test: add testTestTextUploadWithoutAuthentication

--- tests/Feature/Api/Test/TestTextUploadTest.php
+++ tests/Feature/Api/Test/TestTextUploadTest.php
@@ -46,4 +46,22 @@ class TestTextUploadTest extends TestTestCase
         $this->withResponse($response)
             ->assertStatusWithJsonStructure(200, self::TEST_UPLOAD_RESPONSE_JSON_STRUCTURE);
     }
+
+    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
+    public function testTestTextUploadWithoutAuthentication(string $language): void
+    {
+        Storage::fake('public');
+        $file = UploadedFile::fake()->createWithContent(
+            self::FILE_NAME,
+            self::FILE_CONTENT,
+        );
+
+        $response = $this->postJson(self::TEST_UPLOAD_URI, [
+            'language' => $language,
+            'file' => $file,
+        ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithMessage(401, 'Unauthenticated.');
+    }
 }
