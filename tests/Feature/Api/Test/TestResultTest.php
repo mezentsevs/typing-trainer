a333151c67d072a6c102df72d7e48b33d3b35553
mezentsevs <mezentsevs@rambler.ru>
2026-04-20 10:05:12 +0400

    test: add testTestResultSaveWithInvalidSqlInjectionLanguage

--- tests/Feature/Api/Test/TestResultTest.php
+++ tests/Feature/Api/Test/TestResultTest.php
@@ -138,4 +138,18 @@ class TestResultTest extends TestTestCase
         $this->withResponse($response)
             ->assertStatusWithErrorAndMessage(422, 'language', 'The selected language is not supported.');
     }
+
+    public function testTestResultSaveWithInvalidSqlInjectionLanguage(): void
+    {
+        $response = $this->withToken($this->token)
+            ->postJson(self::TEST_RESULT_URI, [
+                'language' => self::INVALID_SQL_INJECTION_LANGUAGE,
+                'time_seconds' => self::TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::ERRORS_COUNT,
+            ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithErrorAndMessage(422, 'language', 'The selected language is not supported.');
+    }
 }
