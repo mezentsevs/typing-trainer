1b5f1f4dd8a875e30c97bec7bfc2e51ccb57f6b9
mezentsevs <mezentsevs@rambler.ru>
2026-04-24 09:39:01 +0400

    test: add testTestResultSaveWithInvalidBoolTimeSeconds

--- tests/Feature/Api/Test/TestResultTest.php
+++ tests/Feature/Api/Test/TestResultTest.php
@@ -195,4 +195,19 @@ class TestResultTest extends TestTestCase
         $this->withResponse($response)
             ->assertStatusWithErrorAndMessage(422, 'time_seconds', 'The time seconds field is required.');
     }
+
+    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
+    public function testTestResultSaveWithInvalidBoolTimeSeconds(string $language): void
+    {
+        $response = $this->withToken($this->token)
+            ->postJson(self::TEST_RESULT_URI, [
+                'language' => $language,
+                'time_seconds' => self::INVALID_BOOL_TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::ERRORS_COUNT,
+            ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithErrorAndMessage(422, 'time_seconds', 'The time seconds field must be an integer.');
+    }
 }
