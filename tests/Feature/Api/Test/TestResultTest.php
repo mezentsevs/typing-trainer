36888d8c20829835ee5997b2e0a3ce14c7d756cf
mezentsevs <mezentsevs@rambler.ru>
2026-04-29 14:05:27 +0400

    test: add testTestResultSaveWithMinTimeSeconds

--- tests/Feature/Api/Test/TestResultTest.php
+++ tests/Feature/Api/Test/TestResultTest.php
@@ -255,4 +255,19 @@ class TestResultTest extends TestTestCase
         $this->withResponse($response)
             ->assertStatusWithErrorAndMessage(422, 'time_seconds', 'The time seconds field must be an integer.');
     }
+
+    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
+    public function testTestResultSaveWithMinTimeSeconds(string $language): void
+    {
+        $response = $this->withToken($this->token)
+            ->postJson(self::TEST_RESULT_URI, [
+                'language' => $language,
+                'time_seconds' => self::MIN_TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::ERRORS_COUNT,
+            ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithJsonStructure(200, self::TEST_RESULT_RESPONSE_JSON_STRUCTURE);
+    }
 }
