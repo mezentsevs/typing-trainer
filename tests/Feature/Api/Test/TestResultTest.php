9f4feb4c704a593718d0d70a13df6514d2aee2c6
mezentsevs <mezentsevs@rambler.ru>
2026-05-27 10:23:12 +0400

    test: add testTestResultSaveWithMaxUnsignedIntegerErrors

--- tests/Feature/Api/Test/TestResultTest.php
+++ tests/Feature/Api/Test/TestResultTest.php
@@ -576,4 +576,19 @@ class TestResultTest extends TestTestCase
         $this->withResponse($response)
             ->assertStatusWithJsonStructure(200, self::TEST_RESULT_RESPONSE_JSON_STRUCTURE);
     }
+
+    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
+    public function testTestResultSaveWithMaxUnsignedIntegerErrors(string $language): void
+    {
+        $response = $this->withToken($this->token)
+            ->postJson(self::TEST_RESULT_URI, [
+                'language' => $language,
+                'time_seconds' => self::TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::MAX_UNSIGNED_INTEGER,
+            ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithJsonStructure(200, self::TEST_RESULT_RESPONSE_JSON_STRUCTURE);
+    }
 }
