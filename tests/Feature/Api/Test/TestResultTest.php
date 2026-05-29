d7c17b87be2f64edca9d46ec8bc1d79f0785a477
mezentsevs <mezentsevs@rambler.ru>
2026-05-29 09:54:41 +0400

    test: add testTestResultSaveWithBelowMinErrors

--- tests/Feature/Api/Test/TestResultTest.php
+++ tests/Feature/Api/Test/TestResultTest.php
@@ -610,4 +610,19 @@ class TestResultTest extends TestTestCase
                 'The errors field must not be greater than ' . self::MAX_UNSIGNED_INTEGER . '.',
             );
     }
+
+    #[DataProviderExternal(CommonDataProvider::class, 'provideSupportedLanguages')]
+    public function testTestResultSaveWithBelowMinErrors(string $language): void
+    {
+        $response = $this->withToken($this->token)
+            ->postJson(self::TEST_RESULT_URI, [
+                'language' => $language,
+                'time_seconds' => self::TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::MIN_ERRORS_COUNT - 1,
+            ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithErrorAndMessage(422, 'errors', 'The errors field must be at least 0.');
+    }
 }
