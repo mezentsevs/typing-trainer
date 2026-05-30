be0be5b4435d6677c233225fcb99a7e7fbb11bd3
mezentsevs <mezentsevs@rambler.ru>
2026-05-30 10:51:11 +0400

    test: add testTestResultSaveValidationErrorHasJsonContentType

--- tests/Feature/Api/Test/TestResultTest.php
+++ tests/Feature/Api/Test/TestResultTest.php
@@ -97,6 +97,19 @@ class TestResultTest extends TestTestCase
             ->assertStatusWithErrorAndMessage(422, 'language', 'The language field is required.');
     }
 
+    public function testTestResultSaveValidationErrorHasJsonContentType(): void
+    {
+        $response = $this->withToken($this->token)
+            ->postJson(self::TEST_RESULT_URI, [
+                'time_seconds' => self::TIME_SECONDS,
+                'speed_wpm' => self::SPEED_WPM,
+                'errors' => self::ERRORS_COUNT,
+            ]);
+
+        $this->withResponse($response)
+            ->assertStatusWithHeaderNameAndValue(422, 'Content-Type', 'application/json');
+    }
+
     public function testTestResultSaveWithNullLanguage(): void
     {
         $response = $this->withToken($this->token)
