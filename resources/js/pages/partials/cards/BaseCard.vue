8bed0b8fa3ac2359b733b6f07c8c903a04d4b0bc
mezentsevs <mezentsevs@rambler.ru>
2026-07-04 10:35:12 +0400

    fix: prevent theme toggle overlapping content on mobile

--- resources/js/pages/partials/cards/BaseCard.vue
+++ resources/js/pages/partials/cards/BaseCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-12 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <slot name="base-logo" />
 
         <div
