02a35cfb5c4996fe32181ebdc469dcd85829a828
mezentsevs <mezentsevs@rambler.ru>
2026-07-21 10:19:13 +0400

    feat: add check icon and animation to success banner

--- resources/js/components/uikit/banners/SuccessBanner.vue
+++ resources/js/components/uikit/banners/SuccessBanner.vue
@@ -1,9 +1,17 @@
 <template>
-    <BaseBanner class="text-green-500">
-        <slot />
-    </BaseBanner>
+    <transition
+        appear
+        enter-active-class="transition duration-200 ease-out"
+        enter-from-class="opacity-0 scale-90"
+        enter-to-class="opacity-100 scale-100">
+        <BaseBanner class="text-green-500 flex items-center gap-1">
+            <CheckIcon class="w-6 h-6" />
+            <slot />
+        </BaseBanner>
+    </transition>
 </template>
 
 <script lang="ts" setup>
 import BaseBanner from '@/components/uikit/banners/BaseBanner.vue';
+import CheckIcon from '@/components/icons/CheckIcon.vue';
 </script>
