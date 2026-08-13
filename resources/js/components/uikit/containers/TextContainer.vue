c9cf8c3a9224672f0eb04198f3dc40c09859921d
mezentsevs <mezentsevs@rambler.ru>
2026-08-13 11:57:02 +0400

    feat: disable text selection in typing container

--- resources/js/components/uikit/containers/TextContainer.vue
+++ resources/js/components/uikit/containers/TextContainer.vue
@@ -1,7 +1,7 @@
 <template>
     <article
         ref="container"
-        class="custom-scroll-container p-2 border border-opacity-50 border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-900 dark:text-gray-300 overflow-y-auto break-words whitespace-pre-wrap rounded-lg shadow-sm">
+        class="custom-scroll-container p-2 border border-opacity-50 border-gray-300 dark:border-gray-700 bg-gray-50 dark:bg-gray-900 dark:text-gray-300 overflow-y-auto break-words whitespace-pre-wrap select-none rounded-lg shadow-sm">
         <slot />
     </article>
 </template>
@@ -11,7 +11,7 @@ import { Ref, ref } from 'vue';
 
 const container: Ref<HTMLElement | null> = ref(null);
 
-const getContainerElement = () => container.value;
+const getContainerElement = (): HTMLElement | null => container.value;
 
 defineExpose({
     getContainerElement,
