cb5217959161f6ab3d481bfa141f33a5fe519706
mezentsevs <mezentsevs@rambler.ru>
2026-09-25 11:35:58 +0400

    fix(languages): remove duplicate chars from id introduction order

--- tests/Providers/Languages/Lesson/IdLessonDataProvider.php
+++ tests/Providers/Languages/Lesson/IdLessonDataProvider.php
@@ -9,22 +9,22 @@ class IdLessonDataProvider extends LanguageLessonDataProvider
 {
     protected const string LANGUAGE_CODE = IdLanguage::CODE;
 
-    protected const string CHARS = 'asdfjkl;qwertyuiophgzxcvbnm,./ASDFJKL;QWERTYUIOPHGZXCVBNM,./1234567890-=!@#$%^&*()_+[]{}|\\:"\'<>?~`';
+    protected const string CHARS = 'asdfjkl;qwertyuiophgzxcvbnm,./ASDFJKLQWERTYUIOPHGZXCVBNM1234567890-=!@#$%^&*()_+[]{}|\\:"\'<>?~`';
 
     protected const array NEW_CHARS_SEQUENCE = [
         1 => 'asdfj',
-        2 => 'kl;qwe',
-        3 => 'rtyuio',
-        4 => 'phgzxc',
-        5 => 'vbnm,./',
-        6 => 'ASDFJKL',
-        7 => ';QWERTY',
-        8 => 'UIOPHGZX',
-        9 => 'CVBNM,./1',
-        10 => '234567890',
-        11 => '-=!@#$%^&*',
-        12 => '()_+[]{}|\\:',
-        13 => '"\'<>?~`',
+        2 => 'kl;qw',
+        3 => 'ertyui',
+        4 => 'ophgzx',
+        5 => 'cvbnm,',
+        6 => './ASDFJ',
+        7 => 'KLQWERT',
+        8 => 'YUIOPHGZ',
+        9 => 'XCVBNM12',
+        10 => '34567890-',
+        11 => '=!@#$%^&*(',
+        12 => ')_+[]{}|\\:"',
+        13 => '\'<>?~`',
         14 => self::CHARS,
         15 => self::CHARS,
         16 => self::CHARS,
