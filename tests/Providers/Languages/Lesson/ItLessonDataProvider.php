9d513f8e5b000be6daff670717ecff533c913493
mezentsevs <mezentsevs@rambler.ru>
2026-09-26 12:02:53 +0400

    feat(languages): align it keyboard with standard layout

--- tests/Providers/Languages/Lesson/ItLessonDataProvider.php
+++ tests/Providers/Languages/Lesson/ItLessonDataProvider.php
@@ -9,22 +9,22 @@ class ItLessonDataProvider extends LanguageLessonDataProvider
 {
     protected const string LANGUAGE_CODE = ItLanguage::CODE;
 
-    protected const string CHARS = 'asdfghjklòàùqwertyuiopè+\\zxcvbnm,.-éìASDFGHJKLÀÒÙQWERTYUIOPÈZXCVBNM;:_ÉÌ1234567890!"£$%&/()=?*|@€[]#~ç°§';
+    protected const string CHARS = 'asdfghjklòàùqwertyuiopè+<zxcvbnm,.-ìéASDFGHJKLÒÀÙQWERTYUIOPÈ>ZXCVBNMÌ1234567890!"£$%&/()=?^*°§;:_\\|\'@#[]{}€ç';
 
     protected const array NEW_CHARS_SEQUENCE = [
         1 => 'asdfgh',
         2 => 'jklòàù',
         3 => 'qwerty',
-        4 => 'uiopè+\\',
+        4 => 'uiopè+<',
         5 => 'zxcvbnm',
-        6 => ',.-éìAS',
-        7 => 'DFGHJKLÀ',
-        8 => 'ÒÙQWERTY',
-        9 => 'UIOPÈZXCV',
-        10 => 'BNM;:_ÉÌ12',
-        11 => '34567890!"£',
-        12 => '$%&/()=?*|@€',
-        13 => '[]#~ç°§',
+        6 => ',.-ìéASD',
+        7 => 'FGHJKLÒÀ',
+        8 => 'ÙQWERTYUI',
+        9 => 'OPÈ>ZXCVB',
+        10 => 'NMÌ1234567',
+        11 => '890!"£$%&/(',
+        12 => ')=?^*°§;:_\\|',
+        13 => '\'@#[]{}€ç',
         14 => self::CHARS,
         15 => self::CHARS,
         16 => self::CHARS,
